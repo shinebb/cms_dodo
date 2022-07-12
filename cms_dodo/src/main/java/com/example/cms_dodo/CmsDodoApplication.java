8da3f15330eae33b6a41331a90612c32c3d1b87a
dohee <ododalrio@gmail.com>
2022-07-12 17:11:51 +0900

    Practice

---
+++ cms_dodo/src/main/java/com/example/cms_dodo/CmsDodoApplication.java
@@ -0,0 +1,13 @@
+package com.example.cms_dodo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CmsDodoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CmsDodoApplication.class, args);
+	}
+
+}
---
+++ cms_dodo/src/test/java/com/example/cms_dodo/CmsDodoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cms_dodo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CmsDodoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
